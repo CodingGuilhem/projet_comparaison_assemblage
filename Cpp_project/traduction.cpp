67e26c23326640840806e6d305b4e6c891343246
CodingGuilhem <biosse.guilhem@gmail.com>
2023-06-22 10:20:46 +0200

    Continuing the project of comparison

--- Cpp_project/traduction.cpp
+++ Cpp_project/traduction.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
+
 int 
 main()
 {
 
-    std::string arg = "gkampi -k 100 -P -o fatsq.fasta --dump=ascii -f -i  test-1000_reads.fastq.gz";
+    string arg = "gkampi -k 100 -P -o fatsq.fasta --dump=ascii -f -i  test-1000_reads.fastq.gz";
     int result = system(&arg[0]);
 
     if (result == 0){
 
     }
-    else { std::cerr<< "Error during the file generation"<< std::endl;}
+    else { cerr<< "Error during the file generation"<< endl;}
     
 }
 
